359e655f4c25c18408ea9f37ce814bb19477ff34
Vinyas Malagaudanavar <vinyasmusic@gmail.com>
2023-03-11 22:52:46 +0530

    Create config

---
+++ app/config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'postgresql://localhost/flask_postgres_test'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
